03cc1e7f7a27ad8e5f35a32504bdffda4242f30c
Daniel Blázquez <danielblazquez312@gmail.com>
2023-01-11 09:54:46 +0100

    cambios en el notebook y en el .R

--- proyecto_gasolineras/test.r
+++ proyecto_gasolineras/test.r
@@ -9,29 +9,6 @@ use_condaenv("base", required = TRUE)
 #Cargamos las funcione del archivo registro.py
 source_python("registro.py")
 #Ejecutamos la función
-registro <- function(user, pwd) {
-  registro(user, pwd)
-}
-
-login <- function(user, pwd) {
-  login(user, pwd)
-}
-
-introducirDatosUser <- function (email, name, coche, modelo, combustible, consumo) {
-  introducirDatosUser(email, name, coche, modelo, combustible, consumo)
-}
-
-getUserID <- function (email) {
-  getUserID(email)
-}
-
-leerDatosUser <- function (id) {
-  leerDatosUser(id)
-}
-
-anadirLocalizacion <- function (id, lat, lon) {
-  anadirLocalizacion(id, lat, lon)
-}
 
 
 
